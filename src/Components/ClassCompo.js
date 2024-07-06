2b0add75e627ce837110c6a52a9440f04be8f401
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2024-07-06 16:01:38 +0530

    class component in React

--- src/Components/ClassCompo.js
+++ src/Components/ClassCompo.js
@@ -3,6 +3,9 @@ import React from 'react'
 import { Component } from 'react';
 
 class componentClass extends Component {
+    constructor(){
+        super()
+    }
     render(){
         return <h5>Hello from Class Component</h5>;
     }
